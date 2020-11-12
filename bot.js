f80fd8b0820f43f1f64a754529f5b04feb274bc0
Andrew <7147407+ha-noh@users.noreply.github.com>
2020-11-11 20:02:27 -0500

    Initial commit for preliminary bot command
    + Bot correctly responds to the specified command and recognizes args. Text repacement currently isn't working as intended

--- bot.js
+++ bot.js
@@ -1,8 +1,30 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
+const config = require('./config.json');
+const { prefix } = require('./config.json');
 require('dotenv').config();
 
 client.login(process.env.BOT_TOKEN);
 client.on('ready', () => {
-    console.log('Pikamee is live!');
+	console.log(`Pikamee is live! Use '${prefix}' to summon me.`);
+});
+
+// command ideas: (1) uwu-ifier (2) insert emojis (take args) in between a message to turn it into pasta (3) tHiS THinG
+client.on('message', message => {
+	// ignore message if it doesn't start with the bot prefix, or if the author is a bot
+	if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if(command === 'uwu') {
+		let convertedMessage = '';
+		for(const arg of args) {
+			arg.replace(/[lr]/g, 'w');
+			arg.replace(/[LR]/g, 'W');
+			convertedMessage = convertedMessage.concat(arg);
+		}
+		message.channel.send(convertedMessage);
+	}
 });
